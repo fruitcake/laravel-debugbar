b36d693b978b594b51f199203d62d801343eb229
Barry vd. Heuvel <barry@fruitcake.nl>
2026-02-06 21:48:22 +0100

    Fix utf-8 encoding

--- src/LaravelDebugbar.php
+++ src/LaravelDebugbar.php
@@ -663,6 +663,15 @@ class LaravelDebugbar extends DebugBar
             $this->data[$name] = $collector->collect();
         }
 
+        // Remove all invalid (non UTF-8) characters
+        array_walk_recursive($this->data, function (&$item): void {
+            if (is_float($item) && !is_finite($item)) {
+                $item = '[NON-FINITE FLOAT]';
+            } else if (is_string($item) && !mb_check_encoding($item, 'UTF-8')) {
+                $item = mb_convert_encoding($item, 'UTF-8', 'UTF-8');
+            }
+        });
+
         if ($this->storage !== null) {
             $this->storage->save($this->getCurrentRequestId(), $this->data);
         }
