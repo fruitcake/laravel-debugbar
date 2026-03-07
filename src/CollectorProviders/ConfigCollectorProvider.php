4198f9e705f2cd3da7ee9f545dd480149ae0f33f
Barry vd. Heuvel <barry@fruitcake.nl>
2026-03-07 08:36:23 +0100

    Tweak config sensitive settings

--- src/CollectorProviders/ConfigCollectorProvider.php
+++ src/CollectorProviders/ConfigCollectorProvider.php
@@ -11,7 +11,7 @@ class ConfigCollectorProvider extends AbstractCollectorProvider
     public function __invoke(array $options): void
     {
         $configCollector = new ConfigCollector();
-        $masked = ['app.key', 'app.previous_keys', '*.*_key', '*.*apikey'];
+        $masked = ['app.key', 'app.previous_keys', '*.*_key', '*.*apikey', '*.*secret*', '*.*password*', '*.*token*'];
         $configCollector->addMaskedKeys(array_merge($masked, $options['masked'] ?? []));
         $this->addCollector($configCollector);
     }
