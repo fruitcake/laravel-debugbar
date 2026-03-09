a56fd9dbb0ea057e7e411458cfd03c6b6b66b039
Barry vd. Heuvel <barry@fruitcake.nl>
2026-03-09 13:39:07 +0100

    Fix mode

--- src/Requests/QueriesExplainRequest.php
+++ src/Requests/QueriesExplainRequest.php
@@ -15,7 +15,7 @@ class QueriesExplainRequest extends FormRequest
             'query' => ['required', 'string'],
             'bindings' => ['nullable', 'array'],
             'hash' => ['required', 'string'],
-            'mode' => ['nullable', 'string', 'in:visual,result'],
+            'mode' => ['nullable', 'string', 'in:explain,visual,result'],
             'format' => ['nullable', 'string'],
         ];
     }
